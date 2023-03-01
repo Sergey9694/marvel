b3039a257485dd9a219e4db0eaa2c567428e34ac
Sergey9694 <drobilenko94@mail.ru>
2023-03-01 15:19:54 +0300

    separated logic and display, added error output and spinner

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
+
 
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
@@ -13,14 +16,27 @@ class RandomChar extends Component {
     }
 
     state = {
-        character: {}
+        character: {},
+        loading: true,
+        error: false
     }
 
     marvelService = new MarvelService(); //  создаем новое свойство внутри RandomChar
 
     //  Метод обновляет state персонажа
     onCharLoaded = (character) => {
-        this.setState({character});
+        this.setState({
+            character, 
+            loading: false
+        });
+    }
+
+    // Метод обработки ошибок
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        });
     }
 
     //  Метод обращается к серверу, получает данные и записывает в state
@@ -29,14 +45,46 @@ class RandomChar extends Component {
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
+            .catch(this.onError);
     }
  
     render() {
-        const {character: {name, description, thumbnail, homepage, wiki}} = this.state;
+        const {character, loading, error} = this.state;
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? <View character={character}/> : null;
+
+// Часть компонента, отвечающая за логику в компоненте
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+                {errorMessage}
+                {spinner}
+                {content}
+                <div className="randomchar__static">
+                    <p className="randomchar__title">
+                        Random character for today!<br/>
+                        Do you want to get to know him better?
+                    </p>
+                    <p className="randomchar__title">
+                        Or choose another one
+                    </p>
+                    <button className="button button__main">
+                        <div className="inner">try it</div>
+                    </button>
+                    <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
+                </div>
+            </div>
+        );
+    }
+}
+
+// Часть компонента, отвечающая за отображение в интерфейсе
+const View = ({character}) => {
+    const {name, description, thumbnail, homepage, wiki} = character;
+
+    return (
+        <div className="randomchar__block">
+            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
                 <div className="randomchar__info">
                     <p className="randomchar__name">{name}</p>
                     <p className="randomchar__descr">{description}</p>
@@ -47,25 +95,10 @@ class RandomChar extends Component {
                         <a href={wiki} className="button button__secondary">
                             <div className="inner">Wiki</div>
                         </a>
-                    </div>
                 </div>
             </div>
-            <div className="randomchar__static">
-                <p className="randomchar__title">
-                    Random character for today!<br/>
-                    Do you want to get to know him better?
-                </p>
-                <p className="randomchar__title">
-                    Or choose another one
-                </p>
-                <button className="button button__main">
-                    <div className="inner">try it</div>
-                </button>
-                <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-            </div>
-            </div>
-        );
-    }
+        </div>
+    )
 }
 
 export default RandomChar;
