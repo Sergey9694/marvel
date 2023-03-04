d1861d9d68613857e4b163680717e090c0e1a3bc
Sergey9694 <drobilenko94@mail.ru>
2023-03-04 12:26:47 +0300

    changed the initialization of the component during mounting, the division of the layout of the component into logic and display

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
+
 
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
@@ -7,36 +10,94 @@ import mjolnir from '../../resources/img/mjolnir.png';
 
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar();
-    }
 
     state = {
-        character: {}
+        character: {},
+        loading: true,
+        error: false,
     }
 
     marvelService = new MarvelService(); //  создаем новое свойство внутри RandomChar
 
+    componentDidMount() { // правильная инициализация компонента
+        this.updateChar();
+    }
+
     //  Метод обновляет state персонажа
     onCharLoaded = (character) => {
-        this.setState({character});
+        this.setState({
+            character, 
+            loading: false,
+        });
+    }
+    // Метод запускает спиннер при обновлении персонажа по нажатию кнопки
+    onCharLoading = () => {
+        this.setState({
+            loading: true,
+            error: false
+        })
+    }
+
+    // Метод обработки ошибок
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        });
     }
 
     //  Метод обращается к серверу, получает данные и записывает в state
     updateChar = () => {
-        const id = Math.floor(Math.random() * (1010789 - 1009146) + 1009146);
+        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.onCharLoading(); // запускает спинер перед запросом к базе данных
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
+            .catch(this.onError);
     }
  
     render() {
-        const {character: {name, description, thumbnail, homepage, wiki}} = this.state;
+        const {character, loading, error} = this.state;
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? <View character={character}/> : null;
+
+// Часть компонента, отвечающая за логику в компоненте
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+                {errorMessage}
+                {spinner}
+                {content}
+                <div className="randomchar__static">
+                    <p className="randomchar__title">
+                        Random character for today!<br/>
+                        Do you want to get to know him better?
+                    </p>
+                    <p className="randomchar__title">
+                        Or choose another one
+                    </p>
+                    <button onClick={this.updateChar} className="button button__main">
+                        <div className="inner">try it</div>
+                    </button>
+                    <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
+                </div>
+            </div>
+        );
+    }
+}
+
+// Часть компонента, отвечающая за отображение в интерфейсе
+const View = ({character}) => {
+    const {name, description, thumbnail, homepage, wiki} = character;
+
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg") {
+        imgStyle = {'objectFit' : 'contain'}
+    } 
+
+    return (
+        <div className="randomchar__block">
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
                 <div className="randomchar__info">
                     <p className="randomchar__name">{name}</p>
                     <p className="randomchar__descr">{description}</p>
@@ -47,25 +108,10 @@ class RandomChar extends Component {
                         <a href={wiki} className="button button__secondary">
                             <div className="inner">Wiki</div>
                         </a>
-                    </div>
                 </div>
             </div>
-            <div className="randomchar__static">
-                <p className="randomchar__title">
-                    Random character for today!<br/>
-                    Do you want to get to know him better?
-                </p>
-                <p className="randomchar__title">
-                    Or choose another one
-                </p>
-                <button className="button button__main">
-                    <div className="inner">try it</div>
-                </button>
-                <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-            </div>
-            </div>
-        );
-    }
+        </div>
+    )
 }
 
 export default RandomChar;
