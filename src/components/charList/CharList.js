bf6a53653e0579ea29d4928ae1739fe868dfc2cf
Sergey9694 <drobilenko94@mail.ru>
2023-03-13 18:41:17 +0300

    functional testing

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -12,7 +12,8 @@ class CharList extends Component {
         loading: true,
         error: false,
         newItemLoading: false,
-        offset: 210
+        offset: 210,
+        charEnded: false
     }
 
     marvelService = new MarvelService();
@@ -36,11 +37,18 @@ class CharList extends Component {
 
     //  Метод обновляет state персонажа
     onCharListLoaded = (newCharList) => {
+        let ended = false;
+        if (newCharList.length < 9) {
+            ended = true;
+        }
+
+
         this.setState(({offset, charList}) => ({
             charList: [...charList, ...newCharList], 
             loading: false,
             newItemLoading: false,
-            offset: offset + 9
+            offset: offset + 9,
+            charEnded: ended
         }));
     }
 
@@ -81,7 +89,7 @@ class CharList extends Component {
 
 
    render () {
-    const {charList, loading, error, newItemLoading, offset} = this.state;
+    const {charList, loading, error, newItemLoading, offset, charEnded} = this.state;
     
     const items = this.renderItems(charList);
 
@@ -96,7 +104,8 @@ class CharList extends Component {
                 {content}
                 <button 
                     className="button button__main button__long"
-                    disabled={newItemLoading}
+                    disabled={newItemLoading} 
+                    style={{'display': charEnded ? 'none' : 'block'}}
                     onClick={() => this.onRequest(offset)}>
                     <div className="inner">load more</div>
                 </button>
