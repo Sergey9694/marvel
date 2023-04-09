9fc82ae1b18742ca0b34c9fde9deed012dea7de5
Sergey9694 <drobilenko94@mail.ru>
2023-04-09 12:18:26 +0300

    separated logic for page routing and rendering of the required component depending on the passed props

--- src/components/animatedRoutes/AnimatedRoutes.js
+++ src/components/animatedRoutes/AnimatedRoutes.js
@@ -6,7 +6,13 @@ import { AnimatePresence } from "framer-motion";
 const Page404 = lazy(() => import("../pages/404"));
 const MainPage = lazy(() => import("../pages/MainPage"));
 const ComicsPage = lazy(() => import("../pages/ComicsPage"));
-const SingleComicPage = lazy(() => import("../pages/SingleComicPage"));
+const SingleComicLayout = lazy(() =>
+    import("../pages/singleComicLayout/SingleComicLayout")
+);
+const SingleCharacterLayout = lazy(() =>
+    import("../pages/singleCharacterLayout/SingleCharacterLayout")
+);
+const SinglePage = lazy(() => import("../pages/SinglePage"));
 
 function AnimatedRoutes() {
     const location = useLocation();
@@ -15,7 +21,24 @@ function AnimatedRoutes() {
             <Routes location={location} key={location.pathname}>
                 <Route path="/" element={<MainPage />} />
                 <Route path="/comics" element={<ComicsPage />} />
-                <Route path="/comics/:comicId" element={<SingleComicPage />} />
+                <Route
+                    path="/comics/:id"
+                    element={
+                        <SinglePage
+                            Component={SingleComicLayout}
+                            dataType="comic"
+                        />
+                    }
+                />
+                <Route
+                    path="/characters/:id"
+                    element={
+                        <SinglePage
+                            Component={SingleCharacterLayout}
+                            dataType="character"
+                        />
+                    }
+                />
                 <Route path="*" element={<Page404 />} />
             </Routes>
         </AnimatePresence>
